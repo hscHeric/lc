126e7ca0226a20f9676375103567da9943f67e8e
hscHeric <hericsilvaho@gmail.com>
2024-03-31 19:51:55 -0300

    153 - update

--- lc-go/153/153.go
+++ lc-go/153/153.go
@@ -1,15 +1,27 @@
 package main
 
 func findMin(nums []int) int {
-	if len(nums) == 1 {
+	n := len(nums)
+	if n == 1 {
 		return nums[0]
 	}
-	middle := len(nums) / 2
-	if middle > 0 && middle < len(nums)-1 && nums[middle] < nums[middle-1] && nums[middle] < nums[middle+1] {
-		return nums[middle]
+	low := 0
+	high := n - 1
+	ans := nums[0]
+	for low <= high {
+		mid := low + (high-low)/2
+		if nums[low] <= nums[high] {
+			ans = min(ans, nums[low])
+			return ans
+		} else {
+			if nums[low] <= nums[mid] {
+				ans = min(ans, nums[mid])
+				low = mid + 1
+			} else {
+				ans = min(ans, nums[mid])
+				high = mid - 1
+			}
+		}
 	}
-	min1 := findMin(nums[:middle])
-	min2 := findMin(nums[middle:])
-
-	return min(min1, min2)
+	return ans
 }
